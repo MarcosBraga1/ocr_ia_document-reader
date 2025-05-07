cfb90be289a23911dca2a5def667e0eb575daa4d
Marcos Braga <dudusilvabraga@hotmail.com>
2025-05-07 02:33:31 -0300

    changing cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // Enable CORS from Next.js
+    origin: 'https://front-ocr-ia-document-reader.onrender.com', // Enable CORS from Next.js
     credentials: true, // Enable cookies to save Token
   });
   app.use(cookieParser());
